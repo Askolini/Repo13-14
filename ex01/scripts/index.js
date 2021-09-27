9280d7d0cd098ab59bd219eaee98c04140322719
Askolini <asmircc@gmail.com>
2021-09-27 06:32:34 +0200

    Finished

---
+++ ex01/scripts/index.js
@@ -0,0 +1,47 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("btn-modal");
+var span = document.getElementsByClassName("btn-close")[0];
+
+function myFunction(id) {
+    modal.style.display = "block";
+    var response = id;
+    var frame = document.getElementById("setSrc");
+    frame.src = setSrc(response);
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+
+function setSrc(response) {
+    if (response == "image1") {
+        return "image01.html";
+    }
+    if (response == "image2") {
+        return "image02.html";
+    }
+    if (response == "image3") {
+        return "image03.html";
+    }
+    if (response == "image4") {
+        return "image04.html";
+    }
+    if (response == "image5") {
+        return "image05.html";
+    }
+    if (response == "image6") {
+        return "image06.html";
+    }
+    if (response == "image7") {
+        return "image07.html";
+    }
+    if (response == "image8") {
+        return "image08.html";
+    }
+    if (response == "image9") {
+        return "image09.html";
+    } {
+        return "image10.html";
+    }
+}
